1a3ea6d99ab6ad8c3c58e97600846927a7a363b9
Anand Dubey(Aadi) <115421478+ananddubey1999@users.noreply.github.com>
2023-01-18 23:07:16 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -2,12 +2,22 @@ import React from 'react'
 import '../styles/App.css';
 const App = () => {
 
-  const handleDoubleClick = (event) =>{
-  
+  const handleInput = (event) =>{
+   console.log('Input in #'+ event.target.id+ ' is ' +event.target.value)
   }
+
+  // do not change id of input elements
   return (
     <div id="main">
-      <button id="dblclick-btn">Double click me</button>
+      <label htmlFor='text-input'>Text Input:- </label>
+      <input onChange={handleInput} id="text-input" type={'text'} />
+
+      <br/>
+      <br/>
+
+      <label htmlFor='num-input'>Number input</label>
+      <input onChange={handleInput} id="num-input"  type={'number'} />
+      <br/>
     </div>
   )
 }
