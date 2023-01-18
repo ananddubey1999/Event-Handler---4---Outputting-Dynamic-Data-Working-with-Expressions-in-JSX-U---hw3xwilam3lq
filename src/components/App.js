92a79df0dd7761c443e0f340050563cb31219537
Anand Dubey(Aadi) <115421478+ananddubey1999@users.noreply.github.com>
2023-01-18 23:18:38 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,26 +1,23 @@
-import React from 'react'
-import '../styles/App.css';
+import React from "react";
+import "../styles/App.css";
 const App = () => {
-
-  const handleInput = (event) =>{
-   console.log('Input in #'+ event.target.id+ ' is ' +event.target.value)
+  const handleDoubleClick = (event) => {
+    console.log("I was double clicked");
+  };
+  function oneClick() {
+    console.log("I was not double clicked");
   }
-
-  // do not change id of input elements
   return (
     <div id="main">
-      <label htmlFor='text-input'>Text Input:- </label>
-      <input onChange={handleInput} id="text-input" type={'text'} />
-
-      <br/>
-      <br/>
-
-      <label htmlFor='num-input'>Number input</label>
-      <input onChange={handleInput} id="num-input"  type={'number'} />
-      <br/>
+      <button
+        id="dblclick-btn"
+        onClick={oneClick}
+        onDoubleClick={handleDoubleClick}
+      >
+        Double click me
+      </button>
     </div>
-  )
-}
-
+  );
+};
 
 export default App;
